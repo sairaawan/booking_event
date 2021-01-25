7eef629107d5c5433e381d9bf2902fe62ebb9677
sairaawan <syraawan@live.com>
2021-01-25 13:18:55 +0000

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 from .models import Artist, Event
 from .forms import ArtistForm
 from django.http import HttpResponseRedirect
-
+from django.core.mail import send_mail
 # Create your views here.
 
 def createform(request): 
+	name=''
+    email=''
+    comment=''
 	form = ArtistForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
+	name= form.cleaned_data.get("name")
+        email= form.cleaned_data.get("email")
+        subject= "New booking"
+        recipients = ['syraawan@live.com']
+        comment= name + " with the email, " + email + ", has made request for new booking";
 		form.save()
-		return HttpResponseRedirect('/success/')
+		send_mail(subject, comment, email, recipients )
+
+		return HttpResponseRedirect('/home/success/')
 	context = {'form': form}
 	return render(request, 'index.html', context)
 
